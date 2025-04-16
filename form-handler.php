d328f2c90cf73386e15c082c59b59a68a0a628e2
charlotte-wan9 <zwan35@wisc.edu>
2025-04-15 22:52:24 -0500

    test php

--- form-handler.php
+++ form-handler.php
@@ -1,31 +1,15 @@
 <?php
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $name = $_POST['name'];
-    $visitor_email = $_POST['email'];
-    $subject = $_POST['subject'];
-    $message = $_POST['message'];
+$email_from = 'no-reply@foundlandmusicaluw.org'; // doesn't need to exist
+$email_subject = 'New Contact Form Submission';
 
-    $email_from = 'no-reply@foundlandmusicaluw.org'; // Use your domain email if you have one
-    $email_subject = 'New Contact Form Submission';
+$email_body = "User Name: $name\n".
+              "User Email: $visitor_email\n".
+              "Subject: $subject\n\n".
+              "Message:\n$message\n";
 
-    $email_body = "User Name: $name\n".
-                  "User Email: $visitor_email\n".
-                  "Subject: $subject\n\n".
-                  "User Message:\n$message\n";
+$headers = "From: $email_from\r\n";
+$headers .= "Reply-To: $visitor_email\r\n";
 
-    $to = 'fdlandmusical@gmail.com';
+mail('fdlandmusical@gmail.com', $email_subject, $email_body, $headers);
 
-    $headers = "From: $email_from\r\n";
-    $headers .= "Reply-To: $visitor_email\r\n";
-
-    if (mail($to, $email_subject, $email_body, $headers)) {
-        header("Location: contact.html?status=success");
-    } else {
-        header("Location: contact.html?status=error");
-    }
-    exit();
-} else {
-    http_response_code(405); // triggers only if someone tries GET
-    echo "405 Method Not Allowed";
-}
 ?>
